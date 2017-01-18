21409358e0306b3aaea34f18d18c50ce9ee5b6c8
刘鹏 <lp_aideals@hotmail.com>
2017-01-18 10:49:16 +0800

    Initial Commit

---
+++ Pretty Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pretty Matchismo
+//
+//  Created by 鹏 刘 on 2017/1/18.
+//  Copyright © 2017年 鹏 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pretty Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pretty Matchismo
+//
+//  Created by 鹏 刘 on 2017/1/18.
+//  Copyright © 2017年 鹏 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
